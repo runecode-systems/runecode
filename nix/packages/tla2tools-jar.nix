699a31964f63d53f02b470d87237977aa393b07b
Zebulon Rouse-Frantzich <zeb@runecode.org>
2026-04-18 12:07:46 -0500

    Updates based on failing checks.
    Signed-off-by: Zebulon Rouse-Frantzich <zeb@runecode.org>

--- nix/packages/tla2tools-jar.nix
+++ nix/packages/tla2tools-jar.nix
@@ -1,6 +1,5 @@
 { pkgs }:
-
-pkgs.runCommand "tla2tools-jar" {} ''
+pkgs.runCommand "tla2tools-jar" { } ''
   mkdir -p "$out"
   cp ${pkgs.tlaplus}/share/java/tla2tools.jar "$out/tla2tools.jar"
   sha256sum "$out/tla2tools.jar" | cut -d' ' -f1 > "$out/tla2tools.jar.sha256"
